da51515ca4c8fa98bc059d35bf3ebdcd48be4d98
A-Awad1 <eng.a.awad1@gmail.com>
2023-03-17 23:34:41 +0200

    finish styling of details page

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Countries Information Website (REST Countries API) (by Vue CLI)
 
-**Project Progress : ![80%](https://progress-bar.dev/80/?title=done)**
+**Project Progress : ![90%](https://progress-bar.dev/90/?title=done)**
 
 This project is advanced challenge from [Frontend Mentor](https://www.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca)
 
--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -7,33 +7,45 @@
         />Back</router-link
       >
       <div class="main-info">
-        <img src="" alt="" />
+        <img
+          :src="countryInfo.flags.png | flagHeight"
+          :alt="`${countryInfo.name} flag`"
+        />
         <div>
-          <h3 v-text="countryInfo.name"></h3>
+          <h2 v-text="countryInfo.name"></h2>
           <div>
-            <span
-              >Native Name:<span v-text="countryInfo.nativeName"></span
-            ></span>
-            <span
-              >Population:<span>
-                {{ countryInfo.population | formatNumber }}
+            <div>
+              <span
+                >Native Name:<span v-text="countryInfo.nativeName"></span
+              ></span>
+              <span
+                >Population:<span>
+                  {{ countryInfo.population | formatNumber }}
+                </span>
               </span>
-            </span>
-            <span>Region:<span v-text="countryInfo.region"></span></span>
-            <span>Sub Region:<span v-text="countryInfo.subregion"></span></span>
-            <span>Capital:<span v-text="countryInfo.capital"></span></span>
-          </div>
-          <div>
-            <span
-              >Top Level Domain:<span></span
-              >{{ countryInfo.topLevelDomain | joinArray }}</span
-            >
-            <span
-              >Currencies:<span>{{ countryCurrencies | joinArray }}</span></span
-            >
-            <span
-              >Languages:<span> {{ countryLanguages | joinArray }}</span></span
-            >
+              <span>Region:<span v-text="countryInfo.region"></span></span>
+              <span
+                >Sub Region:<span v-text="countryInfo.subregion"></span
+              ></span>
+              <span>Capital:<span v-text="countryInfo.capital"></span></span>
+            </div>
+            <div>
+              <span
+                >Top Level Domain:<span>
+                  {{ countryInfo.topLevelDomain | joinArray }}</span
+                ></span
+              >
+              <span
+                >Currencies:<span>{{
+                  countryCurrencies | joinArray
+                }}</span></span
+              >
+              <span
+                >Languages:<span>
+                  {{ countryLanguages | joinArray }}</span
+                ></span
+              >
+            </div>
           </div>
           <div>
             <span>Border Countries:</span>
@@ -99,21 +111,101 @@ export default {
   @extend %main-page-style;
   button {
     padding: 9px 28px;
+    color: $lInput;
     background-color: $lElements;
     border: none;
     box-shadow: 0px 0px 6px -1px $lInput;
     border-radius: $main-border-radius;
     margin: 60px 0;
     svg {
+      color: $lText;
       margin-right: 10px;
     }
   }
-  span {
-    display: block;
-  }
-  span:last-of-type {
-    display: inline;
-    margin-left: 20px;
+  .main-info {
+    display: flex;
+    gap: 70px;
+    @include underMedium {
+      flex-direction: column;
+      align-items: center;
+      gap: 30px;
+    }
+    @include underTablet {
+      align-items: start;
+    }
+    > img {
+      width: 450px;
+      filter: drop-shadow(0px 20px 0px #f8f8f8)
+        drop-shadow(0px -20px 0px #f8f8f8) drop-shadow(20px 0px 0px #f8f8f8)
+        drop-shadow(-20px 0px 0px #f8f8f8);
+      @include underLarge {
+        align-self: center;
+      }
+      @media (max-width: 500px) {
+        width: 100%;
+      }
+    }
+    > div {
+      padding: 20px 0;
+      flex: 1;
+      h2 {
+        color: $lText;
+      }
+      > div:first-of-type {
+        display: flex;
+        justify-content: space-between;
+        @include underLarge {
+          flex-direction: column;
+        }
+        > div {
+          margin: 20px 0;
+          display: flex;
+          flex-direction: column;
+          gap: 10px;
+          > span {
+            color: $lText;
+            font-weight: 600;
+            > span {
+              color: $lInput;
+              &:before {
+                content: " ";
+              }
+            }
+          }
+        }
+      }
+      > div:last-of-type {
+        display: flex;
+        gap: 15px;
+        margin-top: 40px;
+        @include underLarge {
+          flex-direction: column;
+        }
+        @include underMedium {
+          margin-top: 20px;
+        }
+        > span {
+          white-space: nowrap;
+          color: $lText;
+          font-weight: 600;
+        }
+        > div {
+          display: flex;
+          flex-wrap: wrap;
+          gap: 8px 5px;
+          border-radius: $main-border-radius;
+          span {
+            color: $lInput;
+            background-color: $lElements;
+            font-size: 14px;
+            box-shadow: 0px 0px 8px -4px $lInput;
+            min-width: 70px;
+            text-align: center;
+            padding: 4px 6px;
+          }
+        }
+      }
+    }
   }
 }
 </style>
