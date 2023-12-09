7b57d9ef265880b886cc445b6af570527775545e
A-Awad1 <eng.a.awad1@gmail.com>
2023-12-09 07:25:26 +0200

    update some data

--- src/store/index.js
+++ src/store/index.js
@@ -31,6 +31,20 @@ export default new Vuex.Store({
     fetchCountries: function ({ commit }) {
       fetch("https://restcountries.com/v2/all")
         .then((resolved) => resolved.json())
+        // start modifying some data
+        .then((resolve) => {
+          let x = resolve.filter((e) => e.alpha3Code === "PSE")[0];
+          x.name = x.name.split(",", 1)[0];
+          x.currencies = x.currencies.filter((e) => e.code !== "ILS");
+          let y = resolve.filter((e) => e.area === 20770)[0];
+          x.borders = y.borders;
+          resolve.splice(resolve.indexOf(y), 1);
+          resolve
+            .filter((e) => e.borders && e.borders.includes("ISR"))
+            .map((e) => (e.borders[e.borders.indexOf("ISR")] = "PSE"));
+          return resolve;
+        })
+        // end modifying the data
         .then((resolved) => commit("updateCountries", resolved))
         .catch((rejected) => console.log(Error(rejected)));
     },
