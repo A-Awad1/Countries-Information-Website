1b343dce3cb3b3760a14ebcdd03a12418e1a3332
A-Awad1 <eng.a.awad1@gmail.com>
2023-12-09 07:19:42 +0200

    update some data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -32,6 +32,20 @@ export default {
   mounted() {
     fetch("https://restcountries.com/v2/all")
       .then((resolved) => resolved.json())
+      // start modifying some data
+      .then((resolve) => {
+        let x = resolve.filter((e) => e.alpha3Code === "PSE")[0];
+        x.name = x.name.split(",", 1)[0];
+        x.currencies = x.currencies.filter((e) => e.code !== "ILS");
+        let y = resolve.filter((e) => e.area === 20770)[0];
+        x.borders = y.borders;
+        resolve.splice(resolve.indexOf(y), 1);
+        resolve
+          .filter((e) => e.borders && e.borders.includes("ISR"))
+          .map((e) => (e.borders[e.borders.indexOf("ISR")] = "PSE"));
+        return resolve;
+      })
+      // end modifying the data
       .then((resolved) => (this.countries = resolved))
       .catch((rejected) => console.log(Error(rejected)));
   },
