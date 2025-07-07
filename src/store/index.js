e9963bc5a530dae7cbe1b6c437b85a67fbfa2ab6
A-Awad1 <eng.a.awad1@gmail.com>
2025-07-07 14:31:13 +0300

    use json instead of api link:

--- src/store/index.js
+++ src/store/index.js
@@ -26,8 +26,9 @@ export default createStore({
   },
   actions: {
     fetchCountries: function ({ commit }) {
-      fetch("https://restcountries.com/v2/all")
+      fetch("/json/db.json")
         .then((resolved) => resolved.json())
+        .then((resolve) => resolve.data)
         // start modifying some data
         .then((resolve) => {
           let x = resolve.filter((e) => e.alpha3Code === "PSE")[0];
